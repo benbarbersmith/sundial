040de88f5f92e9bd2af7171d1644379e2f0ad495
Ben Smith <ben@benjaminasmith.com>
2013-04-10 14:07:34 +0100

    Add a minimal HTML UI to the application.

--- lib/sundial.rb
+++ lib/sundial.rb
@@ -9,17 +9,36 @@ disable :raise_errors
 converter = Converter.new
 
 get '/' do
-  redirect 'https://github.com/benjaminasmith/sundial'
+  # Numbers will be provided via a ?convert= parameter.
+  if params[:convert].nil?
+    # If no number has been provided, show a clear index page.
+    erb :index, :locals => {
+      :input => nil
+    }
+  else
+    # If a number has been provided, redirect to a friendly URL.
+    redirect URI::encode("/" + params[:convert])
+  end
 end
 
 get '/:input' do |input|
+  # Get timezone results for the input string.
   results = converter.convert(input)
-  # Return a readable output string.
-  results.map do |r|
+  # Build a readable output string.
+  time = results.map do |r|
     "#{r[:time]} #{r[:timezone]} (GMT #{'+' if r[:offset] > 0}#{r[:offset]})"
   end.join(" or ")
+  # Render template to present outputs neatly.
+  erb :result, :locals => {
+    :input => input, 
+    :time => time,
+  }
 end
 
 error do
-  'An error occured: ' + request.env['sinatra.error'].message
+  # If in doubt, render an error message.
+  erb :error, :locals => {
+    :input => nil,
+    :error => 'An error occured: ' + request.env['sinatra.error'].message,
+  }
 end
